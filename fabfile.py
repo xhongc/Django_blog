a985a9e44822c7e70ed6711596a3140b05b39ebe
408737515@qq.com <408737515@qq.com>
2017-09-05 14:01:27 +0800

    Signed-off-by: 408737515@qq.com <408737515@qq.com>

--- fabfile.py
+++ fabfile.py
@@ -3,21 +3,23 @@ from fabric.operations import sudo
 
 GIT_REPD ="https://github.com/xhongc/Django_blog.git"
 
-env.user ='104.225.154.56'
-env.password ='chao123456789..'
+env.user ='chao'
+env.password ='xiehongchao'
 
 env.hosts =['xhongc.cc']
 
 env.port ='28298'
 
+
 def deploy():
-    source_folder = 'home/chao/sites/demo.xhongc.cc/Django_blog'
-    run('cd %s && git pull'% source_folder)
+    source_folder = '/home/chao/sites/demo.xhongc.cc/Django_blog'
+    run('cd ..')
+    run('cd %s && git stash && git pull && git stash pop'% source_folder)
     run('''
-    cd {} &&
-    ../env/bin/pip install -r requirements.txt &&
-    ../env/bin/python3 manage.py collectstatic --noinput &&
-    ../env/bin/python3 manage.py migrate
+    cd {0} &&
+    /home/chao/sites/demo.xhongc.cc/env/bin/pip install -r requirements.txt &&
+    /home/chao/sites/demo.xhongc.cc/env/bin/python3 manage.py collectstatic --noinput &&
+    /home/chao/sites/demo.xhongc.cc/env/bin/python3 manage.py migrate
     '''.format(source_folder))
 
     sudo('restart gunicorn-xhongc.cc')
